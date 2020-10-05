26fee43a9b1fa1f0346fd59925f6c45d86d5a225
abhi <abhijeetvc7@gmail.com>
2020-10-05 20:29:59 +0530

    Initial commit

---
+++ src/test/java/com/visclient/visserviceone/VisserviceoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visclient.visserviceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisserviceoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
